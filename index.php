a33e498158227a666e9c33d8084b76303b3cca72
Toyyib Lawal <toyibe25@gmail.com>
2023-07-23 00:27:41 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Srex Web</title>
+		<link rel="stylesheet" href="styles/style.css" />
+	</head>
+	<body>
+		<nav>
+			<h1 class='rubik'>Srex</h1>
+			<?php echo 'hey'?>
+		</nav>
+	</body>
+</html>
